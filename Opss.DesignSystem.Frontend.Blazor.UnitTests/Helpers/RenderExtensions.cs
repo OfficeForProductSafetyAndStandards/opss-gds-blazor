bfc7fbe6cc7e9ef706f8254731d694c7692073b1
Richard priddy <richard.priddy@triad.co.uk>
2024-11-25 14:27:23 +0000

    Use CascadingValue to simplify child Id values
    We've had to alter tests for any element that uses CascadingValue as Blazor adds hidden unicode character \uFEFF when CascadingValue is used.

---
+++ Opss.DesignSystem.Frontend.Blazor.UnitTests/Helpers/RenderExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Opss.DesignSystem.Frontend.Blazor.UnitTests.Helpers
+{
+	public static class RenderExtensions
+	{
+        /// <summary>
+        /// bUnit MarkupMatches doesn't recognise the hidden unicode character \uFEFF that is added
+        /// to markup by Blazor when using CascadingValue. Replace this with nothing so bUnit passes.
+        /// </summary>
+        /// <param name="component">The component to extract the Markup from</param>
+        /// <returns>Markup without hidden \uFEFF character</returns>
+        public static string bUnitMarkup(this IRenderedComponent<Bunit.Rendering.ContainerFragment> component)
+        {
+            return component.Markup.Replace("\uFEFF", "");
+        }
+	}
+}
