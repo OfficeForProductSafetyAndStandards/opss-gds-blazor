7a26c7c42005e2776b1f8b14b586407c08a1fb1a
Richard priddy <richard.priddy@triad.co.uk>
2024-11-28 15:58:13 +0000

    Include Date Input

---
+++ Opss.DesignSystem.Frontend.Blazor.Components/Classes/DateParts.cs
@@ -0,0 +1,8 @@
+﻿namespace Opss.DesignSystem.Frontend.Blazor.Components.Classes;
+
+public static class DateParts
+{
+    public const string Day = "Day";
+    public const string Month = "Month";
+    public const string Year = "Year";
+}
