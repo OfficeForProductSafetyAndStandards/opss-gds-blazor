6596061e45941bb908c1642996cc0ea94d3ed959
Richard priddy <richard.priddy@triad.co.uk>
2024-12-05 09:42:36 +0000

    Id attribute isn't required by everything
    Remove Id from GdsViewModel and add only to those components that need it.

--- Opss.DesignSystem.Frontend.Blazor.Components/Models/GdsViewModel.cs
+++ Opss.DesignSystem.Frontend.Blazor.Components/Models/GdsViewModel.cs
@@ -5,12 +5,6 @@ namespace Opss.DesignSystem.Frontend.Blazor.Components.Models;
 public abstract class GdsViewModel : GdsAttributes
 {
     /// <summary>
-    /// Set the id of the element
-    /// </summary>
-    [Parameter]
-    public string? Id { get; set; }
-
-    /// <summary>
     /// Classes to add to the element
     /// </summary>
     [Parameter]
