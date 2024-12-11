607cb84028dcfb956c71c735e1d483267a323acf
Richard priddy <richard.priddy@triad.co.uk>
2024-12-11 10:06:32 +0000

    Update to fix card tests
    Still have an update to resolve the bracketed text for list row action.

--- Opss.DesignSystem.Frontend.Blazor.Components/Classes/SummaryListClasses.cs
+++ Opss.DesignSystem.Frontend.Blazor.Components/Classes/SummaryListClasses.cs
@@ -3,4 +3,6 @@
 public static class SummaryListClasses
 {
     public const string NoBorder = "govuk-summary-list--no-border";
+
+    public const string RowNoBorder = "govuk-summary-list__row--no-border";
 }
