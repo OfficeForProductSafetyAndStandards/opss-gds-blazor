e731164ac585d31df1621a6be241d53f051d4b56
Scotty Wakefield <scott.wakefield@triad.co.uk>
2025-01-21 14:04:31 +0000

    changed pragma directive to attribute

--- Opss.DesignSystem.Frontend.Blazor.Showcase/Pages/Error.cshtml.cs
+++ Opss.DesignSystem.Frontend.Blazor.Showcase/Pages/Error.cshtml.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
-#pragma warning disable CA1812
+
 namespace Opss.DesignSystem.Frontend.Showcase.Pages
 {
+    [SuppressMessage("Performance", "CA1812: Avoid uninstantiated internal classes", Justification = "This is the viewmodel for Error.cshml, so it is instantiated, but by asp.net.")]
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
     internal sealed class ErrorModel : PageModel
