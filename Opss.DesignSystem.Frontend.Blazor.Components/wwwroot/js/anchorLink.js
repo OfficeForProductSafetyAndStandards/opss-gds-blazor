a5b067f60be9ff1289e7bb6f225c6d444f17a578
tobytryad <tobi.omojayogbe@triad.co.uk>
2025-03-31 10:13:22 +0100

    PVB-676: remove js

--- Opss.DesignSystem.Frontend.Blazor.Components/wwwroot/js/anchorLink.js
+++
@@ -1,9 +0,0 @@
-﻿window.anchorLink = {
-    scrollIntoView: (targetId) => {
-        var targetElement = document.getElementById(targetId);
-        if (targetElement) {
-            targetElement.scrollIntoView();
-            window.location.hash = targetId;
-        }
-    }
-}
